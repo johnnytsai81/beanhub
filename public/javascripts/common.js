0ee902a7c3b60a4bbc0664d894547a3772da3e98
Anthony Tsai <johnnytsai81@gmail.com>
2022-09-03 13:46:58 +0800

    start

---
+++ public/javascripts/common.js
@@ -0,0 +1,10 @@
+const anlottie = lottie.loadAnimation({
+    container: svgContainer,
+    animType: 'svg',
+    loop: true,
+    autoplay: false,
+    path: './dashboard.json',
+});
+    
+ 
+anlottie.play();
