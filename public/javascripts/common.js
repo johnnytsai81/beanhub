60662587ea52da335144c39a0cfc5cbc7220a1ac
Anthony Tsai <johnnytsai81@gmail.com>
2022-09-17 23:19:22 +0800

    123

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -5,19 +5,19 @@ if (svgContainer1){
     animType: 'svg',
     loop: true,
     autoplay: false,
-    path: './json/key_visual.json',
+    path: '/public/json/key_visual.json',
   });
   anlottie1.play();
 }
 
-const svgContainer2 = document.querySelector('#svgContainer1')
+const svgContainer2 = document.querySelector('#svgContainer2')
 if (svgContainer2){
   const anlottie2 = lottie.loadAnimation({
     container: svgContainer2,
     animType: 'svg',
     loop: true,
     autoplay: false,
-    path: './json/feature1.json',
+    path: '/public/json/feature1.json',
   });
   anlottie2.play();
 }
