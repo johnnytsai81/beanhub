5e85ce25d690959bdf5c7619da0f34a45b2cf42c
Anthony Tsai <johnnytsai81@gmail.com>
2022-09-17 23:11:39 +0800

    new animation

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,10 +1,23 @@
-const anlottie = lottie.loadAnimation({
+const svgContainer1 = document.querySelector('#svgContainer1')
+if (svgContainer1){
+  const anlottie1 = lottie.loadAnimation({
     container: svgContainer,
     animType: 'svg',
     loop: true,
     autoplay: false,
-    path: './dashboard.json',
-});
-    
- 
-anlottie.play();
+    path: './json/key_visual.json',
+  });
+  anlottie1.play();
+}
+
+const svgContainer2 = document.querySelector('#svgContainer1')
+if (svgContainer2){
+  const anlottie2 = lottie.loadAnimation({
+    container: svgContainer,
+    animType: 'svg',
+    loop: true,
+    autoplay: false,
+    path: './json/feature1.json',
+  });
+  anlottie2.play();
+}
