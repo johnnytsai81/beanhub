713b27da95226e40a0003b3a8a639984958a9f79
Anthony Tsai <johnnytsai81@gmail.com>
2022-09-19 23:36:59 +0800

    new animation

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -10,14 +10,39 @@ if (svgContainer1){
   anlottie1.play();
 }
 
-const svgContainer2 = document.querySelector('#svgContainer2')
-if (svgContainer2){
-  const anlottie2 = lottie.loadAnimation({
-    container: svgContainer2,
-    animType: 'svg',
-    loop: true,
-    autoplay: false,
-    path: './feature1.json',
+//圖往右滑的效果
+function svgInview() {
+  $('#svgContainer2').each(function () {
+      $(this).on('inview', function (event, isInView) {
+          if (isInView) {
+            const anlottie2 = lottie.loadAnimation({
+              container: svgContainer2,
+              animType: 'svg',
+              loop: false,
+              autoplay: false,
+              path: './no_repeat.json',
+            });
+            anlottie2.play();
+            const anlottie3 = lottie.loadAnimation({
+              container: svgContainer3,
+              animType: 'svg',
+              loop: true,
+              autoplay: false,
+              path: './repeat.json',
+            });
+            anlottie3.play();
+          }
+      });
   });
-  anlottie2.play();
 }
+
+// ScrollLottie({
+//   target: '#svgContainer2',
+//   path: './feature1.json', 
+//   duration: 4, 
+//   speed: 'medium'
+// })
+
+$(document).ready(function () {
+  svgInview();
+});
